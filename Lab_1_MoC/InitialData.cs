fbb7420d7257b3dc2b57e6e37b324230883b2a83
Elina <evpatosha@gmail.com>
2021-10-18 00:49:25 +0300

    update initial data

--- Lab_1_MoC/InitialData.cs
+++ Lab_1_MoC/InitialData.cs
@@ -9,18 +9,20 @@ namespace Lab_1_MoC
         static protected double[] M;
         static protected double[] k;
         static protected int[,] C;
-
-    public void SetMAndK(string path)
+        string defaultpath = "C:/Users/ellun/source/repos/Lab_1_MoC/Lab_1_MoC/Data/";
+    public void SetMAndK(string filename)
         {
+            string path = defaultpath + filename;
             var lines = File.ReadLines(path).ToList();
             M = new double[lines[0].Length];
             k = new double[lines[0].Length];
-            M = lines[0].Split(',').Select(s => Double.Parse(s.Replace(".", ","))).ToArray();
-            k = lines[1].Split(',').Select(s => Double.Parse(s.Replace(".", ","))).ToArray();
+            M = lines[0].Split(',').Select(s => double.Parse(s.Replace(".", ","))).ToArray();
+            k = lines[1].Split(',').Select(s => double.Parse(s.Replace(".", ","))).ToArray();
         }
 
-        public void SetC(string path)
+        public void SetC(string filename)
         {
+            string path = defaultpath + filename;
             var lines = File.ReadLines(path).ToList();
             C = new int[lines.Count, lines.Count];
             for (int line = 0; line < lines.Count; line++)
