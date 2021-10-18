dc37d4bd13a56c426b21234577efcc03d2060f98
Elina <evpatosha@gmail.com>
2021-10-18 08:59:23 +0300

    add Display class

---
+++ Lab_1_MoC/Display.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_1_MoC
+{
+    class Display
+    {
+        static public void DisplayTwoDimArray(double[,] arr)
+        {
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    Console.Write("|" + arr[i, j] + "\t");
+
+                }
+                Console.WriteLine();
+            }
+
+        }
+        static public void DisplayList(List<int> list)
+        {
+            foreach (var element in list)
+            {
+                Console.Write(element + " ");
+            }
+        }
+
+    }
+}
