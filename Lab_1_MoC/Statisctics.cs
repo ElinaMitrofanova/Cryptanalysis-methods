6ddd4faae3c519d5149ff344f4faf72e7ebddbd6
Elina <evpatosha@gmail.com>
2021-10-18 08:06:21 +0300

    refactor class and add AvgDeterministic

--- Lab_1_MoC/Statisctics.cs
+++ Lab_1_MoC/Statisctics.cs
@@ -8,38 +8,22 @@ namespace Lab_1_MoC
 {
     class Statisctics: InitialData
     {
-        public double[] Pr_CipherText()
+        double[] prC = new double[C.GetLength(0)];
+        double[,] prMC = new double[C.GetLength(0), C.GetLength(1)];
+        public Statisctics()
         {
-            double[] prС = new double[C.GetLength(0)];
             for (int i = 0; i < C.GetLength(0); i++)
             {
                 for (int j = 0; j < C.GetLength(1); j++)
                 {
-                    prС[C[i, j]] += Math.Round(M[j] * k[i],4);
-                    prС[C[i, j]] = Math.Round(prС[C[i, j]], 4);
-
+                    prC[C[i, j]] += Math.Round(M[j] * k[i], 4);
+                    prMC[C[i, j], j] += Math.Round(M[j] * k[i], 4);
                 }
             }
-            return prС;
         }
 
-        public double[,] Pr_MC()
-        {
-            double[,] prMC = new double[C.GetLength(0), C.GetLength(1)];
-            for (int i = 0; i < C.GetLength(0); i++)
-            {
-                for (int j = 0; j < C.GetLength(1); j++)
-                {
-                    prMC[C[i, j],j] += M[j] * k[i];
-                    prMC[C[i, j], j] = Math.Round(prMC[C[i, j], j], 4);
-                }
-            }
-            return prMC;
-        }
          public double[,] PrConditional_MC()
         {
-            double [] prC = Pr_CipherText();
-            double[,] prMC= Pr_MC();
             double[,] prMC_Cond = new double[C.GetLength(0), C.GetLength(1)];
             for (int i = 0; i < C.GetLength(0); i++)
             {
@@ -51,10 +35,10 @@ namespace Lab_1_MoC
             return prMC_Cond;
         }
 
-        public List<double> DeterministicFunction()
+        public List<int> DeterministicFunction()
         {
             double[,] prMC_Cond = PrConditional_MC();
-            List<double> maxList = new List<double>();
+            List<int> maxList = new List<int>();
             for (int i = 0; i < prMC_Cond.GetLength(0); i++)
             {
                 List<double> tempList = new List<double>();
@@ -67,11 +51,17 @@ namespace Lab_1_MoC
             }
             return maxList;
         }
-        void StochasticMatrix()
-        {
-            List<double> deterministicFunction = DeterministicFunction();
-            double[,] stMatrix= new double[C.GetLength(0), C.GetLength(1)];
 
+        public double AvgDeterministic()
+        {
+            double[,] prMC_Cond = PrConditional_MC();
+            double avgLoss =0;
+            List<int> deterministicFunction = DeterministicFunction();
+            for (int i = 0; i < prC.Length; i++)
+            {
+                avgLoss += prC[i] * (1 - prMC_Cond[i, deterministicFunction[i]]);
+            }
+            return avgLoss;
         }
     }
 }
